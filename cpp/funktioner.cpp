8054f33b50f971ad74ddfeb1b30416a20ffb819f
Aron Granberg <aron@Arons-MacBook-Pro.local>
2012-09-06 09:30:02 +0200

    Wrote some stuff on funktions. Also added comments here and there.

---
+++ cpp/funktioner.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+//Definera funktionen abs.
+//Funktionen tar 1 argument, en integer
+//och returnerar en integer
+int abs(int x){
+	if (x < 0) {
+		return -x;
+	} else {
+		return x;
+	}
+}
+
+int main(){
+	int tal;
+	cout <<  "Skriv ett tal: " << endl;
+	cin >> tal;
+	cout << "Beloppet av ditt tal är " << abs(x) << endl;
+}
--- cpp/villkor2.cpp
+++ cpp/villkor2.cpp
@@ -6,7 +6,12 @@ int main(){
     int a;
     cout << "Skriv in ett heltal: ";
     cin >> a;
-    int originalA = a; //Vi sparar ursprungsvärdet
+    //Vi sparar ursprungsvärdet
+    int originalA = a;
+    
+    //Notera att vi använder två '=' tecken.
+    //Endast 1 '=' tecken är tilldelning
+    //2 stycken kollar om två uttryck är identiska och ger true eller false
     if(a == 1){
         a = a;
     }
