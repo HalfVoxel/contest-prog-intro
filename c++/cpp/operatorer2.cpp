f7cee61dadae91732fbf6421baaa7be2236a12da
Johan Sannemo <johan.sannemo@gmail.com>
2013-08-28 22:18:49 +0000

    Clarifications

--- c++/cpp/operatorer2.cpp
+++ c++/cpp/operatorer2.cpp
@@ -7,18 +7,18 @@ int main(){
     //Likhetsoperatorn ger värdet sann om de två jämförda värdena är samma
     //Observera att likhetsoperatorn består av dubbla likhetstecken, till
     // skillnad från tilldelningsoperatorn.
-    bool a = tal == 5;
+    bool a = (tal == 5);
 
     //På motsatt sätt fungerar olikhetsoperatorn
-    bool b = tal != 4;
+    bool b = (tal != 4);
 
     //Större-än-operatorn kollar om ett tal är större eller mindre än ett annat
-    bool c = tal > 2;
-    bool d = tal < 2;
+    bool c = (tal > 2);
+    bool d = (tal < 2);
 
     //Dessa kan kombineras till större-eller-lika-med-operatorn:
-    bool e = tal <= 5;
-    bool f = tal >= 6;
+    bool e = (tal <= 5);
+    bool f = (tal >= 6);
 
     //Negationsoperatorn ! inverterar ett booleskt värde
     //Dvs !sant blir till falskt, och !falskt blir till sant
@@ -27,18 +27,18 @@ int main(){
 
     //Konjunktion ("och") finns också i C++ i form av &&-operatorn
     //Det gäller att a&&b == sant om och endast om både a och b är sanna
-    bool i = true && true;
-    bool j = true && false;
+    bool i = (true && true);
+    bool j = (true && false);
 
     //Disjunktion finns i två former. Dels den inklusiva disjunktionen
     //"eller" (ofta kallad "or") som skrivs ||...
-    bool k = true || false;
-    bool l = false || false;
+    bool k = (true || false);
+    bool l = (false || false);
 
     //...men också i form av exklusiv disjunktion (ofta kallad "xor"):
     //det ena eller det andra, men inte båda. Detta skrivs med ^
-    bool m = true ^ false;
-    bool n = true ^ true;
+    bool m = (true ^ false);
+    bool n = (true ^ true);
 
     //När ett booleskt värde skrivs ut betyder 0 falskt och 1 sant
     cout << a << ' ' << b << ' ' << c << ' ' << d << ' ';
